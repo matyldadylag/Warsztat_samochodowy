ee8211d9da5c4ef803de554b359d0418dfa07713
matyldadylag <matylda.dylag@outlook.com>
2025-05-13 13:51:38 +0200

    Add project files.

---
+++ Warsztat_samochodowy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Warsztat_samochodowy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
