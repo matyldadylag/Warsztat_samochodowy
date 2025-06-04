c280fbc1d208265a708de617013564306a0e4049
matyldadylag <matylda.dylag@outlook.com>
2025-06-04 10:03:00 +0200

    Zmieniłam wersję na NET 8.0

---
+++ Warsztat_samochodowy/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Warsztat_samochodowy.Models;
+using Warsztat_samochodowy.Data;
+using Warsztat_samochodowy.DTOs;
+
+namespace Warsztat_samochodowy.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly WorkshopDbContext _context;
+        public CustomerController(WorkshopDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = _context.Customers
+                .Select(c => new CustomerListDto
+                {
+                    Id = c.Id,
+                    FullName = c.FullName
+                })
+                .ToList();
+
+            return View(customers);
+        }
+    }
+}
---
+++ Warsztat_samochodowy/DTOs/CustomerListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Warsztat_samochodowy.DTOs
+{
+    public class CustomerListDto
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = default!;
+    }
+}
