a8e067b8a61be56056495040842c9f86fe4f896d
matyldadylag <matylda.dylag@outlook.com>
2025-06-08 18:23:54 +0200

    Rejestracja z poziomu konta admina. Logowanie/rejestracja dokończone

--- Warsztat_samochodowy/Controllers/AccountController.cs
+++ Warsztat_samochodowy/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Warsztat_samochodowy.Models;
 using Warsztat_samochodowy.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Warsztat_samochodowy.Controllers
 {
@@ -10,35 +12,62 @@ namespace Warsztat_samochodowy.Controllers
     {
         private readonly UserManager<ApplicationUserModel> _userManager;
         private readonly SignInManager<ApplicationUserModel> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(UserManager<ApplicationUserModel> userManager, SignInManager<ApplicationUserModel> signInManager)
+        public AccountController(
+            UserManager<ApplicationUserModel> userManager,
+            SignInManager<ApplicationUserModel> signInManager,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public IActionResult Register() => View();
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminRegister()
+        {
+            // Pobieramy nazwy ról z bazy i przekazujemy do widoku
+            var roles = _roleManager.Roles.Select(r => r.Name).ToList();
+            ViewBag.Roles = roles;
+
+            return View();
+        }
 
         [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Register(RegisterViewModel model)
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AdminRegister(AdminRegisterViewModel model)
         {
-            if (!ModelState.IsValid) return View(model);
+            if (!ModelState.IsValid)
+            {
+                // Gdy walidacja nie przejdzie, też musimy przekazać listę ról
+                ViewBag.Roles = _roleManager.Roles.Select(r => r.Name).ToList();
+                return View(model);
+            }
 
-            var user = new ApplicationUserModel { UserName = model.Email, Email = model.Email };
+            var user = new ApplicationUserModel
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
             var result = await _userManager.CreateAsync(user, model.Password);
 
             if (result.Succeeded)
             {
-                await _userManager.AddToRoleAsync(user, "Recepcjonista");
-                await _signInManager.SignInAsync(user, isPersistent: false);
-                return RedirectToAction("Index", "Home");
+                if (!await _userManager.IsInRoleAsync(user, model.Role))
+                    await _userManager.AddToRoleAsync(user, model.Role);
+
+                TempData["Success"] = "Użytkownik został dodany.";
+                return RedirectToAction("AdminRegister");
             }
 
             foreach (var error in result.Errors)
                 ModelState.AddModelError(string.Empty, error.Description);
+
+            // W przypadku błędów też zwracamy listę ról do widoku
+            ViewBag.Roles = _roleManager.Roles.Select(r => r.Name).ToList();
 
             return View(model);
         }
@@ -61,10 +90,17 @@ namespace Warsztat_samochodowy.Controllers
             return View(model);
         }
 
+        [Authorize]
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
         }
     }
 }
---
+++ Warsztat_samochodowy/ViewModels/AdminRegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Warsztat_samochodowy.ViewModels
+{
+    public class AdminRegisterViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password), Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+
+}
